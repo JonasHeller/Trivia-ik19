b620ae23ef2f6969a70ca4d686f73babed428960
morris van bergem <imorrris@me.com>
2018-01-29 00:13:50 +0000

    new

--- static/script.js
+++ static/script.js
@@ -1,5 +1,19 @@
-$("#submit").keyup(function(event) {
+// $("#submit").keyup(function(event) {
+//     if (event.keyCode === 13) {
+//         $("#submit").click();
+//     }
+// });
+
+function checkDo() {
     if (event.keyCode === 13) {
         $("#submit").click();
     }
-});
+}
+
+// $(document).keypress(function(e) {
+//     if (e.which == 13) {
+//         alert('enter key is pressed');
+//         $("#submit").click();
+//     }
+// });
+
